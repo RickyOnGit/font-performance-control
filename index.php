deb66dd2ab48d03ac1050ebc11e9db8066ba2143
Riccardo Castagna <3315954155@libero.it>
2018-07-03 18:46:22 +0200

    Example File

--- index.php
+++ index.php
@@ -49,7 +49,6 @@ body {
 margin: 0px;
 padding: 0px;
 }
-
 h1, p {font-family:"Montserrat Alternates";}
 h1{
 font-size: 18px;
@@ -66,36 +65,35 @@ width:100%;
 </style>
 </head>
   <body>
-<div class="title">
-   <h1>Controlling Font Performance with font-display</h1>
-</div>
-
-<p>This php package solves the problem to charge an external font resource 
-  with the <strong>"controlling font performance"</strong> using <strong>font-display</strong> options.<br>
-  This option is useful, to fast load the external font and in general to have the control over browser behavioral and, in particular, over how the
-  browser has to load the external font.<br><br>  
-  At the moment is no possible to add at any external font resource the font-display options,<br> 
-  when you call the query, for example, of google font:<br>
-  "https://fonts.googleapis.com/css?family=Montserrat+Alternates%3A300%2C500" <br>
-  and you wish to add the param: "&font-display=fallback" <br>
-  like this: "https://fonts.googleapis.com/css?family=Montserrat+Alternates%3A300%2C500&font-display=fallback" <br> 
-  As far as I know, Google has not yet solved this issue and, to work, 
-  the "font-display" option must be inserted as param within the css function @font-face{}.<br>   
-  A possible way is, to use the php cURL extension to manipulate this external resourse and add 
-  the font-display option param inside the css function.<br><br>
-  With this tecnique is also possible to manipulate, more or less, everything; others external resource like: 
-  google-map, file css, file js, jquery, what do you want ...<br><br> 
-  How you can see, I have used the curl_multi_init() istead of curl_init(), this to make you free to change, if you wish, the php class and 
-  to add more than one font resource.<br><br>
-  <strong>CLASS USAGE:</strong><br> 
-  $ref= new Fontperformance;<br>
-  $ref->fontdisplay("https://fonts.googleapis.com/css?family=Montserrat+Alternates%3A300%2C500","fallback");<br>
-  Where param 1 is a string, is the link to external font resource, in this example by google font.<br>
-  param 2 is a string, is the performance controlling option. Possible values are:<br>
-  auto | block | swap | fallback | optional<br> 
-  For a complete reference guide and option usage please consult:<br> 
-    <a href="https://developers.google.com/web/updates/2016/02/font-display">Controlling Font Performance with font-display
-    </a>  
-</p> 
- </body>
+   <div class="title">
+     <h1>Controlling Font Performance with font-display</h1>
+   </div>
+    <p>This php package solves the problem to charge an external font resource 
+        with the <strong>"controlling font performance"</strong> using <strong>font-display</strong> options.<br>
+        This option is useful, to fast load the external font and in general to have the control over browser behavioral and, in particular, over how the
+        browser has to load the external font.<br><br>  
+        At the moment is no possible to add at any external font resource the font-display options,<br> 
+        when you call the query, for example, of google font:<br>
+        "https://fonts.googleapis.com/css?family=Montserrat+Alternates%3A300%2C500" <br>
+        and you wish to add the param: "&font-display=fallback" <br>
+        like this: "https://fonts.googleapis.com/css?family=Montserrat+Alternates%3A300%2C500&font-display=fallback" <br> 
+        As far as I know, Google has not yet solved this issue and, to work, 
+        the "font-display" option must be inserted as param inside the css function @font-face{}.<br>   
+        A possible way is, to use the php cURL extension to manipulate this external resourse and add 
+        the font-display option param inside the css function.<br><br>
+        With this tecnique is also possible to manipulate, more or less, everything; others external resource like: 
+        google-map, file css, file js, jquery, what do you want ...<br><br> 
+        How you can see, I have used the curl_multi_init() istead of curl_init(), this to make you free to change, if you wish, the php class and 
+        to add more than one font resource.<br><br>
+        <strong>CLASS USAGE:</strong><br> 
+        $ref= new Fontperformance;<br>
+        $ref->fontdisplay("https://fonts.googleapis.com/css?family=Montserrat+Alternates%3A300%2C500","fallback");<br>
+        Where param 1 is a string, is the link to external font resource, in this example by google font.<br>
+        param 2 is a string, is the performance controlling option. Possible values are:<br>
+         auto | block | swap | fallback | optional<br> 
+        For a complete reference guide and option usage please consult:<br> 
+        <a href="https://developers.google.com/web/updates/2016/02/font-display">Controlling Font Performance with font-display
+        </a>  
+    </p> 
+  </body>
 </html>
