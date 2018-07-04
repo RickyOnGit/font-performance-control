380f2fbe699c4faf9e34a50b7a8cdf38868ed98c
Riccardo Castagna <3315954155@libero.it>
2018-07-04 09:04:49 +0200

    EXAMPLE FILE

--- index.php
+++ index.php
@@ -75,31 +75,32 @@ width:100%;
    </div>
     <p>This php package solves the problem to charge an external font resource 
         with the <strong>"controlling font performance"</strong> using <strong>font-display</strong> options.<br>
-        This option is useful, to fast load the external font and in general to have the control over browser behavioral and, in particular, over how the
+        This option is useful, to fast load the external font and in general to have the control over browser behavioral and,<br> in particular, over how the
         browser has to load the external font.<br><br>  
         At the moment is no possible to add at any external font resource the font-display options,<br> 
-        when you call the query, for example, of google font:<br>
+        when you call the query, for example, through google font API:<br>
         "https://fonts.googleapis.com/css?family=Montserrat+Alternates%3A300%2C500" <br>
-        and you wish to add the param: "&font-display=fallback" <br>
-        like this: "https://fonts.googleapis.com/css?family=Montserrat+Alternates%3A300%2C500&font-display=fallback" <br> 
+        and you wish to add the descriptor param: "&font-display=fallback" <br>
+        making something like this query: "https://fonts.googleapis.com/css?family=Montserrat+Alternates%3A300%2C500&font-display=fallback" <br><br> 
         As far as I know, Google has not yet solved this issue and, to work, 
-        the "font-display" option must be inserted as param inside the css function @font-face{}.<br>   
+        the "font-display" descriptor option must be inserted as param inside the css function @font-face{}.<br>   
         A possible way is, to use the php cURL extension to manipulate this external resourse and add 
-        the font-display option param inside the css function. And also, very important thing, if the external resource is charged by server side, 
-        the client side gain. Working on localhost, obviously, the thing is imperceptible because on localhost, server and client are the same device.<br><br>
-        With this tecnique is also possible to manipulate, more or less, everything; others external resource like: 
-        google-map, file css, file js, jquery, what do you want ...<br><br> 
-        How you can see, I have used the curl_multi_init() istead of curl_init(), this to make you free to change, if you wish, the php class and 
-        to add more than one font resource.<br><br>
+        the font-display option param inside the css function.<br> And also, very important thing, if the external resource is charged by server side, 
+        the client side gain.<br> Working on localhost, obviously, the thing is imperceptible because on localhost, server and client are the same device.<br><br>
+        With this tecnique is also possible to manipulate, more or less, everything; others external resource and API like:<br> google-map, file css, 
+        file js, jquery, what do you want, with the aim to lighten the client's operations and also to hide any API keys.<br><br> 
+        How you can see, I have used the curl_multi_init() istead of curl_init(),<br> this because, probably, in the future I will add some 
+        other external resource to be loaded simultaneously.<br><br>
         <strong>CLASS USAGE:</strong><br> 
-        $ref= new Fontperformance;<br>
-        $ref->fontdisplay("https://fonts.googleapis.com/css?family=Montserrat+Alternates%3A300%2C500","fallback");<br>
-        Where param 1 is a string, is the link to external font resource, in this example by google font.<br>
+        <strong>$ref= new Fontperformance;</strong><br>
+        <strong>$ref->fontdisplay("https://fonts.googleapis.com/css?family=Montserrat+Alternates%3A300%2C500","fallback");</strong><br><br>
+        Where param 1 is a string, is the link to external font resource, in this example through google font api.<br>
         param 2 is a string, is the performance controlling option. Possible values are:<br>
-         auto | block | swap | fallback | optional<br> 
-        For a complete reference guide and option usage please consult:<br> 
+        <strong>auto | block | swap | fallback | optional</strong><br><br> 
+        For a complete reference guide about font-display descriptors please consult:<br> 
         <a href="https://developers.google.com/web/updates/2016/02/font-display">Controlling Font Performance with font-display
-        </a>  
+        </a><br><br>
+        <a href="https://api.whatsapp.com/send?phone=393315954155">info & contacts</a>  
     </p> 
   </body>
 </html>
