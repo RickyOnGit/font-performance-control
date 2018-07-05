2d18c7463d14be47b1a8e74b224745e5a5136701
Riccardo Castagna <3315954155@libero.it>
2018-07-05 13:01:50 +0200

    PHP MAIN CLASS

--- lib/class.fontperformance.php
+++ lib/class.fontperformance.php
@@ -23,7 +23,7 @@
 * With this tecnique is also possible to manipulate, more or less, everything; others external resource and API like:     *
 * google-map, file css, file js, jquery, what do you want, with the aim to lighten the client's operations and also       *
 * to hide any API keys.                                                                                                   *                                                            
-* How you can see, I have used the curl_multi_init() istead of curl_init(), this because, probably, in the future         *
+* How you can see, I have used the curl_multi_init(), this because, probably, in the future                               *
 * I will add some other external resource to be loaded simultaneously, changing something in the main class.              *
 * CLASS USAGE:                                                                                                            *
 * $ref= new Fontperformance;                                                                                              *
