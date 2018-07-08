7e420eee6e137f1ae1d4bd3d62efbc25d003d4f3
Riccardo Castagna <3315954155@libero.it>
2018-07-09 00:04:37 +0200

    EXAMPLE FILE

--- index.php
+++ index.php
@@ -26,8 +26,8 @@
 * the font-display option param inside the css function, moreover, in this way, the API keys, when these are necessary,   *
 * will be hidden.                                                                                                         *                                                            
 * With this php class there is also the possibility (VIEW:ADVANCED CLASS USAGE EXAMPLE N°2 FILE: INDEX_2.PHP)             *
-* to load the external fonts (TEN MAXIMUM) all together simultaneously, it is faster but it must be used with extreme     *
-* caution and with common sense, example file: index_2.php.                                                               *
+* to load the external fonts (TEN MAXIMUM, PLEASE) all together simultaneously, it is faster but it must be used          *
+* with extreme  caution and with common sense, example file: index_2.php.                                                 *
 * Ten fonts simultaneously is enough AND, PLEASE, NOT FROM ONLY ONE SERVER if we                                          *
 * want to avoid to commute this demo into a cyber attack to google servers, and if we want to prevent                     *
 * google killing me and you; I joke, obviously, I joke ... ;)                                                             *
@@ -53,9 +53,10 @@
 *                                                                                                                         *
 * ADVANCED CLASS USAGE EXAMPLE N°2 FILE: INDEX_2.PHP (SIMULTANEOUSLY)                                                     *
 * $ref= new Fontperformance;                                                                                              *
-* $ref->multi_simul_fontdisplay("link_1","option_1","link_2","option_2","link_n","option_n", ... );                       *
-* where the params: link_1, link_2, link_n ... are "strings", are the links to external font resources                    *
-* and where the paramas: option_1, option_2, option_n ... are the related options                                         *
+* $apilink = array("link_to_font_api_1","link_to_font_api_n", ....);                                                      *
+* $ref->multi_simul_fontdisplay($apilink,"fallback" );                                                                    *
+* where the params1 is an array with all links to the font api                                                            *
+* and where the param 2 is a string, is the performance controlling option. Possible values are:                          *
 * auto | block | swap | fallback | optional , this will return an array with all fonts.                                   *                                             *
 * PLEASE DO NOT FOLLOW MY BAD EXAMPLE, DON'T LOAD MORE THAN TWO MAXIMUM DIFFERENT FONTS SIMULTANEOUSLY                    *
 *                                                                                                                         *
@@ -130,7 +131,7 @@ width:100%;
  the font-display option param inside the css function, moreover, in this way, the API keys, when these are necessary,<br>   
  will be hidden.</strong><br><br>                                                                                                                                                                    
  With this php class there is also the possibility (VIEW:ADVANCED CLASS USAGE EXAMPLE N&deg;2 FILE: INDEX_2.PHP)<br>             
- to load the external fonts (TEN MAXIMUM) all together simultaneously, it is faster but it must be used with extreme<br>     
+ to load the external fonts (TEN MAXIMUM, PLEASE) all together simultaneously, it is faster but it must be used with extreme<br>     
  caution and with common sense, example file: index_2.php.<br>                                                               
  Ten fonts simultaneously is enough<strong> AND, PLEASE, NOT FROM ONLY ONE SERVER</strong><br> if we                                          
  want to avoid to commute this demo into a cyber attack to google servers, and if we want to prevent<br>                     
@@ -159,9 +160,10 @@ width:100%;
                                                                                                                          
 <strong> ADVANCED CLASS USAGE EXAMPLE N&deg;2 FILE: INDEX_2.PHP (SIMULTANEOUSLY)</strong><br>                                                     
  $ref= new Fontperformance;<br>                                                                                              
- $ref->multi_simul_fontdisplay("link_1","option_1","link_2","option_2","link_n","option_n", ... );<br>                       
- where the params: link_1, link_2, link_n ... are "strings", are the links to external font resources<br>                    
- and where the paramas: option_1, option_2, option_n ... are the related options<br>                                         
+ $apilink = array("link_to_font_api_1","link_to_font_api_n", ....);<br>                                                      
+ $ref->multi_simul_fontdisplay($apilink,"fallback" );<br>                                                                    
+ where the params1 is an array with all links to the font api<br>                                                            
+ and where the param 2 is a string, is the performance controlling option. Possible values are:<br>                                         
  auto | block | swap | fallback | optional , this will return an array with all fonts.<br>                                                                                
 <strong> PLEASE DO NOT FOLLOW MY BAD EXAMPLE, DON'T LOAD MORE THAN TWO MAXIMUM DIFFERENT FONTS SIMULTANEOUSLY</strong><br><br> 
         For a complete reference guide about font-display descriptors please consult:<br> 
