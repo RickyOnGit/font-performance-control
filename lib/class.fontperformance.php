7bc9ecc6c3f6a6b9a90cfe1fc8107f9838f0c575
Riccardo Castagna <3315954155@libero.it>
2018-07-09 00:03:53 +0200

    PHP MAIN CLASS

--- lib/class.fontperformance.php
+++ lib/class.fontperformance.php
@@ -26,8 +26,8 @@
 * the font-display option param inside the css function, moreover, in this way, the API keys, when these are necessary,   *
 * will be hidden.                                                                                                         *                                                            
 * With this php class there is also the possibility (VIEW:ADVANCED CLASS USAGE EXAMPLE N°2 FILE: INDEX_2.PHP)             *
-* to load the external fonts (TEN MAXIMUM) all together simultaneously, it is faster but it must be used with extreme     *
-* caution and with common sense, example file: index_2.php.                                                               *
+* to load the external fonts (TEN MAXIMUM, PLEASE) all together simultaneously, it is faster but it must be used          *
+* with extreme  caution and with common sense, example file: index_2.php.                                                 *
 * Ten fonts simultaneously is enough AND, PLEASE, NOT FROM ONLY ONE SERVER if we                                          *
 * want to avoid to commute this demo into a cyber attack to google servers, and if we want to prevent                     *
 * google killing me and you; I joke, obviously, I joke ... ;)                                                             *
@@ -53,9 +53,10 @@
 *                                                                                                                         *
 * ADVANCED CLASS USAGE EXAMPLE N°2 FILE: INDEX_2.PHP (SIMULTANEOUSLY)                                                     *
 * $ref= new Fontperformance;                                                                                              *
-* $ref->multi_simul_fontdisplay("link_1","option_1","link_2","option_2","link_n","option_n", ... );                       *
-* where the params: link_1, link_2, link_n ... are "strings", are the links to external font resources                    *
-* and where the paramas: option_1, option_2, option_n ... are the related options                                         *
+* $apilink = array("link_to_font_api_1","link_to_font_api_n", ....);                                                      *
+* $ref->multi_simul_fontdisplay($apilink,"fallback" );                                                                    *
+* where the params1 is an array with all links to the font api                                                            *
+* and where the param 2 is a string, is the performance controlling option. Possible values are:                          *
 * auto | block | swap | fallback | optional , this will return an array with all fonts.                                   *                                             *
 * PLEASE DO NOT FOLLOW MY BAD EXAMPLE, DON'T LOAD MORE THAN TWO MAXIMUM DIFFERENT FONTS SIMULTANEOUSLY                    *
 *                                                                                                                         *
@@ -142,177 +143,61 @@ $fontdisplayoption = $this->fontfaceresolve($fontoption,$font);
 return $fontdisplayoption;                
 }
 
-public function multi_simul_fontdisplay($fontlink_1,$option_1,$fontlink_2='',$option_2='',$fontlink_3='',$option_3='',$fontlink_4='',$option_4='',
-$fontlink_5='',$option_5='', $fontlink_6='',$option_6='', $fontlink_7='', $option_7='',$fontlink_8='',$option_8='',
-$fontlink_9='',$option_9='',$fontlink_10='',$option_10=''){
 
-$fontoption_1=$this->optionresolve($option_1);
+public function multi_simul_fontdisplay($paramfont,$paramoption){
+if ((is_array($paramfont))&&(isset($paramoption))) {
 $ssl = $this->sslresolve();
-$externalfont_1 = $this->linkresolve($fontlink_1);
-$ch1 = curl_init();  
-$this->set_option($ch1, $ssl.$externalfont_1);
-$mh = curl_multi_init();
-curl_multi_add_handle($mh, $ch1);  
-
-if (($option_2!='')&&($fontlink_2!='')){
-$fontoption_2=$this->optionresolve($option_2);
-$externalfont_2 = $this->linkresolve($fontlink_2);
-$ch2 = curl_init();  
-$this->set_option($ch2, $ssl.$externalfont_2);
-curl_multi_add_handle($mh, $ch2);  
-}
-
-if (($option_3!='')&&($fontlink_3!='')){
-$fontoption_3=$this->optionresolve($option_3);
-$externalfont_3 = $this->linkresolve($fontlink_3);
-$ch3 = curl_init();  
-$this->set_option($ch3, $ssl.$externalfont_3);
-curl_multi_add_handle($mh, $ch3);  
-}
-
-if (($option_4!='')&&($fontlink_4!='')){
-$fontoption_4=$this->optionresolve($option_4);
-$externalfont_4 = $this->linkresolve($fontlink_4);
-$ch4 = curl_init();  
-$this->set_option($ch4, $ssl.$externalfont_4);
-curl_multi_add_handle($mh, $ch4);  
-}
-
-if (($option_5!='')&&($fontlink_5!='')){
-$fontoption_5=$this->optionresolve($option_5);
-$externalfont_5 = $this->linkresolve($fontlink_5);
-$ch5 = curl_init();  
-$this->set_option($ch5, $ssl.$externalfont_5);
-curl_multi_add_handle($mh, $ch5);  
-}
-
-if (($option_6!='')&&($fontlink_6!='')){
-$fontoption_6=$this->optionresolve($option_6);
-$externalfont_6 = $this->linkresolve($fontlink_6);
-$ch6 = curl_init();  
-$this->set_option($ch6, $ssl.$externalfont_6);
-curl_multi_add_handle($mh, $ch6);  
-}
-
-if (($option_7!='')&&($fontlink_7!='')){
-$fontoption_7=$this->optionresolve($option_7);
-$externalfont_7 = $this->linkresolve($fontlink_7);
-$ch7 = curl_init();  
-$this->set_option($ch7, $ssl.$externalfont_7);
-curl_multi_add_handle($mh, $ch7);  
-}
-
-if (($option_8!='')&&($fontlink_8!='')){
-$fontoption_8=$this->optionresolve($option_8);
-$externalfont_8 = $this->linkresolve($fontlink_8);
-$ch8 = curl_init();  
-$this->set_option($ch8, $ssl.$externalfont_8);
-curl_multi_add_handle($mh, $ch8);  
-}
-
-if (($option_9!='')&&($fontlink_9!='')){
-$fontoption_9=$this->optionresolve($option_9);
-$externalfont_9 = $this->linkresolve($fontlink_9);
-$ch9 = curl_init();  
-$this->set_option($ch9, $ssl.$externalfont_9);
-curl_multi_add_handle($mh, $ch9);  
-}
-
-if (($option_10!='')&&($fontlink_10!='')){
-$fontoption_10=$this->optionresolve($option_10);
-$externalfont_10 = $this->linkresolve($fontlink_10);
-$ch10 = curl_init();  
-$this->set_option($ch10, $ssl.$externalfont_10);
-curl_multi_add_handle($mh, $ch10);  
-}
+$n = count($paramfont);
+$q = count($paramoption);
+if(($n>0)&&($q==1)){
 $running = null;
-do {
-curl_multi_exec($mh, $running);
+$ch = array();
+$font = array();
+$fontoption = array();
+$multifont = array();
+
+$fontoption[0]=$this->optionresolve($paramoption);
+$externalfont[0] = $this->linkresolve($paramfont[0]);
+$ch[0] = curl_init();
+$this->set_option($ch[0], $ssl.$externalfont[0]);
+$mh = curl_multi_init();
+curl_multi_add_handle($mh, $ch[0]);
+ 
+$m=1;
+foreach ($paramfont as $k => $fontvalue){
+if($k!=0){          
+$fontoption[$m]=$this->optionresolve($paramoption);
+$externalfont[$m] = $this->linkresolve($fontvalue);
+$ch[$m] = curl_init();  
+$this->set_option($ch[$m], $ssl.$externalfont[$m]); 
+curl_multi_add_handle($mh, $ch[$m]); 
+$m++;
+}
+}
+
+do { 
+curl_multi_exec($mh, $running); 
 } while ($running);
 
-curl_multi_remove_handle($mh, $ch1);
-
-if (isset($ch2)){
-curl_multi_remove_handle($mh, $ch2);
-}
-if (isset($ch3)){
-curl_multi_remove_handle($mh, $ch3);
-}
-if (isset($ch4)){
-curl_multi_remove_handle($mh, $ch4);
-}
-if (isset($ch5)){
-curl_multi_remove_handle($mh, $ch5);
-}
-if (isset($ch6)){
-curl_multi_remove_handle($mh, $ch6);
-}
-if (isset($ch7)){
-curl_multi_remove_handle($mh, $ch7);
-}
-if (isset($ch8)){
-curl_multi_remove_handle($mh, $ch8);
-}
-if (isset($ch9)){
-curl_multi_remove_handle($mh, $ch9);
-}
-if (isset($ch10)){
-curl_multi_remove_handle($mh, $ch10);
+$x = 0;
+foreach ($ch as $chvalue){
+$font[$x] = curl_multi_getcontent($chvalue);
+curl_multi_remove_handle($mh, $chvalue);
+$x++;
 }
 
+$y = 0;
+foreach($fontoption as $fontoptionval ){
+foreach($font as $fontvalor ){
+$multifont[$y] = $this->fontfaceresolve($fontoptionval,$fontvalor);
+$y++; 
+}
+} 
 curl_multi_close($mh);
-
-$font_1 = curl_multi_getcontent($ch1);
-$fontdisplayoption_1 = $this->fontfaceresolve($fontoption_1,$font_1);   
-
-if (isset($ch2)){
-$font_2 = curl_multi_getcontent($ch2);
-$fontdisplayoption_2 = $this->fontfaceresolve($fontoption_2,$font_2); 
-} else {$fontdisplayoption_2 ='';}
-
-if (isset($ch3)){
-$font_3 = curl_multi_getcontent($ch3);
-$fontdisplayoption_3 = $this->fontfaceresolve($fontoption_3,$font_3); 
-} else {$fontdisplayoption_3 ='';}
-
-if (isset($ch4)){
-$font_4 = curl_multi_getcontent($ch4);
-$fontdisplayoption_4 = $this->fontfaceresolve($fontoption_4,$font_4); 
-} else {$fontdisplayoption_4 ='';}
-
-if (isset($ch5)){
-$font_5 = curl_multi_getcontent($ch5);
-$fontdisplayoption_5 = $this->fontfaceresolve($fontoption_5,$font_5); 
-} else {$fontdisplayoption_5 ='';}
-
-if (isset($ch6)){
-$font_6 = curl_multi_getcontent($ch6);
-$fontdisplayoption_6 = $this->fontfaceresolve($fontoption_6,$font_6); 
-} else {$fontdisplayoption_6 ='';}
-
-if (isset($ch7)){
-$font_7 = curl_multi_getcontent($ch7);
-$fontdisplayoption_7 = $this->fontfaceresolve($fontoption_7,$font_7); 
-} else {$fontdisplayoption_7 ='';}
-
-if (isset($ch8)){
-$font_8 = curl_multi_getcontent($ch8);
-$fontdisplayoption_8 = $this->fontfaceresolve($fontoption_8,$font_8); 
-} else {$fontdisplayoption_8 ='';}
-
-if (isset($ch9)){
-$font_9 = curl_multi_getcontent($ch9);
-$fontdisplayoption_9 = $this->fontfaceresolve($fontoption_9,$font_9); 
-} else {$fontdisplayoption_9 ='';}
-
-if (isset($ch10)){
-$font_10 = curl_multi_getcontent($ch10);
-$fontdisplayoption_10 = $this->fontfaceresolve($fontoption_10,$font_10); 
-} else {$fontdisplayoption_10 ='';}
-
-$multifont = array($fontdisplayoption_1,$fontdisplayoption_2,$fontdisplayoption_3,$fontdisplayoption_4,$fontdisplayoption_5,$fontdisplayoption_6,
-$fontdisplayoption_7,$fontdisplayoption_8,$fontdisplayoption_9,$fontdisplayoption_10);
+}
+}
 return $multifont; 
 }
+
 }
 ?>
