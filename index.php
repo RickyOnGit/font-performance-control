79e3c8e6f102901071087a5d07f2bbf89f5851f7
Riccardo Castagna <3315954155@libero.it>
2018-07-12 15:04:26 +0200

    EXAMPLE FILE

--- index.php
+++ index.php
@@ -26,24 +26,24 @@
 * the font-display option param inside the css function, moreover, in this way, the API keys, when these are necessary,   *
 * will be hidden.                                                                                                         *                                                            
 * With this php class there is also the possibility (VIEW:ADVANCED CLASS USAGE EXAMPLE N°2 FILE: INDEX_2.PHP)             *
-* to load the external fonts (TEN MAXIMUM, PLEASE) all together simultaneously, it is faster but it must be used          *
-* with extreme  caution and with common sense, example file: index_2.php.                                                 *
-* Ten fonts simultaneously is enough AND, PLEASE, NOT FROM ONLY ONE SERVER if we                                          *
-* want to avoid to commute this demo into a cyber attack to google servers, and if we want to prevent                     *
-* google killing me and you; I joke, obviously, I joke ... ;)                                                             *
-* But, obviously, no one load ten fonts simultaneously in only one page. For testing, in the example N°2, fl. index_2.php *
-* by forcing the test, I have loaded seven different font simultaneously from only one server,                            *
+* to load the external fonts all together simultaneously,  index_2.php.                                                   *
+* To avoid changing this demo into a cyber attack to google servers, and if we want to prevent                            *
+* google killing me and you; I joke, obviously, I joke ... ;), in the example N 2,the font files are stored locally and   *
+* since, usually, we don't change fonts every day, why we have to connect each time to this external resources ?          *
+* is it necessary .... ? My thought is no, it is not necessary, instead, it need to set the .htaccess file with           *
+* a long time cache for the font files.                                                                                   *
+* In the EXAMPLE TWO I have loaded seven different font simultaneously from only one server,                              *
 * seven are too much, but I did it only for testing to show                                                               *
-* that also in this case, with seven different fonts loaded simultaneously, all text remains visible with                 *
+* that also in this case, with seven different fonts, all text remains visible with                                       *
 * the set up of the font-display during web fonts load.                                                                   *
-*                                                                                                                         *
+* Simultaneously is only the check of the server to the exeternal font resource because the resources are downloaded only *                                                                                                 
+* if locally don't exists.                                                                                                *
 * To avoid the critical request chain  https://developers.google.com/web/tools/lighthouse/audits/critical-request-chains  *
 * I have set up to defer (load asynchronously) the style with a small escamotage:                                         *
-* <style media="none" onload="if(media!='all')media='all'" >                                                              *
-* The ligthhouse report of this demo with seven fonts loaded simultaneously is:                                           *
-* https://googlechrome.github.io/lighthouse/viewer/?gist=798a57975a8555b6417a09446ce50b09                                 *                                                                                      *
+* <style media="none" onload="if(media!='all')media='all'" >                                                              *                                                                                       *
 *                                                                                                                         *
-* SIMPLE CLASS USAGE EXAMPLE N°1 FILE: INDEX.PHP (NO SIMULTANEOUSLY)                                                      *
+* SIMPLE CLASS USAGE EXAMPLE N°1 FILE:INDEX.PHP (FONT-DISPLAY YES, NO SIMULTANEOUSLY CHECK,                               *
+* NO FONT FILES ARE STORED LOCALLY, .HTACCESS CACHE YES);                                                                 *
 * $ref= new Fontperformance;                                                                                              *
 * $font_1 = $ref->fontdisplay("link_to_font_api","fallback");                                                             *
 * $font_2 = $ref->fontdisplay("link_to_font_api","auto");                                                                 *
@@ -51,17 +51,18 @@
 * param 2 is a string, is the performance controlling option. Possible values are:                                        *
 * auto | block | swap | fallback | optional                                                                               *
 *                                                                                                                         *
-* ADVANCED CLASS USAGE EXAMPLE N°2 FILE: INDEX_2.PHP (SIMULTANEOUSLY)                                                     *
+* ADVANCED CLASS USAGE EXAMPLE N°2 FILE:INDEX_2.PHP (FONT-DISPLAY YES, SIMULTANEOUSLY CHECK YES,                          *
+* ALL FONT FILES ARE STORED LOCALLY IF LOCALLY DO NOT EXIST, .HTACCESS CACHE YES);                                        *
 * $ref= new Fontperformance;                                                                                              *
 * $apilink = array("link_to_font_api_1","link_to_font_api_n", ....);                                                      *
 * $ref->multi_simul_fontdisplay($apilink,"fallback" );                                                                    *
-* where the params1 is an array with all links to the font api                                                            *
+* where the params1 is an array with all links to the font api, it's good, also, for only one font,                       *
 * and where the param 2 is a string, is the performance controlling option. Possible values are:                          *
-* auto | block | swap | fallback | optional , this will return an array with all fonts.                                   *                                             *
-* PLEASE DO NOT FOLLOW MY BAD EXAMPLE, DON'T LOAD MORE THAN TWO MAXIMUM DIFFERENT FONTS SIMULTANEOUSLY                    *
+* auto | block | swap | fallback | optional , this will return an array with all fonts.                                   * 
 *                                                                                                                         *
 * For a complete reference guide about font-display descriptor values please consult:                                     *
 * https://developers.google.com/web/updates/2016/02/font-display                                                          *
+* https://www.w3.org/TR/css-fonts-4/#font-display-font-feature-values                                                     *
 **************************************************************************************************************************/
 include_once("./lib/class.fontperformance.php"); 
 $ref= new Fontperformance;
@@ -104,7 +105,7 @@ width:100%;
   <body>
    <div class="title">
      <h1 >Controlling Font Performance with font-display, example n&deg;1</h1>
-     <h2 >NO SIMULTANEOUSLY</h2>
+     <h2 >FONT-DISPLAY YES, NO SIMULTANEOUSLY CHECK,<br> NO FONT FILES ARE STORED LOCALLY</h2>
    </div>
     <p class="montserrat"> This php package solves the problem to charge an external font resource<br>                                                 
  with the <strong>"controlling font performance"</strong> using font-display options.<br><br>                                                     
@@ -129,24 +130,27 @@ width:100%;
                   
  <strong> A possible way is, to use the PHP to manipulate this external resourse and add<br>                                          
  the font-display option param inside the css function, moreover, in this way, the API keys, when these are necessary,<br>   
- will be hidden.</strong><br><br>                                                                                                                                                                    
+ will be hidden.</strong><br><br>
+                                                                                                                                                                     
  With this php class there is also the possibility (VIEW:ADVANCED CLASS USAGE EXAMPLE N&deg;2 FILE: INDEX_2.PHP)<br>             
- to load the external fonts (TEN MAXIMUM, PLEASE) all together simultaneously, it is faster but it must be used with extreme<br>     
- caution and with common sense, example file: index_2.php.<br>                                                               
- Ten fonts simultaneously is enough<strong> AND, PLEASE, NOT FROM ONLY ONE SERVER</strong><br> if we                                          
- want to avoid to commute this demo into a cyber attack to google servers, and if we want to prevent<br>                     
- google killing me and you; I joke, obviously, I joke ... ;)<br>                                                             
- But, obviously, no one load ten fonts simultaneously in only one page. For testing, in the example N&deg;2, fl. index_2.php<br> 
- by forcing the test, I have loaded seven different font simultaneously from only one server,<br>                            
+ to load the external fonts all together simultaneously,  index_2.php.<br>                                                   
+ To avoid changing this demo into a cyber attack to google servers, and if we want to prevent<br>                          
+ google killing me and you; I joke, obviously, I joke ... ;), in the example N&deg;2,the font files are stored locally and<br>   
+ since, usually, we don't change fonts every day, why we have to connect each time to this external resources ?<br>          
+ is it necessary .... ? My thought is no, it is not necessary, instead, it need to set the .htaccess file with<br>           
+ a long time cache for the font files.<br>                                                                                   
+ In the EXAMPLE TWO I have loaded seven different font simultaneously from only one server,<br>                              
  seven are too much, but I did it only for testing to show<br>                                                               
- that also in this case, with seven different fonts loaded simultaneously, all text remains visible with<br>                 
- the set up of the font-display during web fonts load.<br><br>
+ that also in this case, with seven different fonts, all text remains visible with<br>                 
+ the set up of the font-display during web fonts load.<br>                                                                   
+ Simultaneously is only the check of the server to the exeternal font resource because the resources are downloaded only<br>                                                                                                  
+ if locally don't exists.<br><br>
                                                                     
  To avoid the <a href="https://developers.google.com/web/tools/lighthouse/audits/critical-request-chains">critical request chain</a><br>  
  I have set up to defer (load asynchronously) the style with a small escamotage:<br>                                         
  style media="none" onload="if(media!='all')media='all'"<br><br>                                                              
  Here you can see the ligthhouse reports, about performances, of these two demo,<br>example N&deg;1 with two fonts not loaded simultaneously and<br>
- example N&deg;2 with seven fonts loaded simultaneously;<br><br>  
+ example N&deg;2 with seven different fonts;<br><br>  
  example N&deg;1 Emulated Nexus 5X, Throttled Fast 3G network,<br> Network throttling: 562,5 ms HTTP RTT, 1.474,6 Kbps down, 675 Kbps up (DevTools)<br>
  CPU throttling: 4x slowdown (DevTools):<br> 
  <a href="https://googlechrome.github.io/lighthouse/viewer/?gist=b316fc892210f82dfcf56f5285c75ee6">lighthouse performance report N 1</a><br>
@@ -155,12 +159,13 @@ width:100%;
  <a href="https://googlechrome.github.io/lighthouse/viewer/?gist=e79ffd09199fecaa5ecd35f84f3d32e8">lighthouse performance report N 1</a><br><br>                                          
  example N&deg;2 Emulated Nexus 5X, Throttled Fast 3G network,<br> Network throttling: 562,5 ms HTTP RTT, 1.474,6 Kbps down, 675 Kbps up (DevTools)<br>
  CPU throttling: 4x slowdown (DevTools):<br>
- <a href="https://googlechrome.github.io/lighthouse/viewer/?gist=b24ca1a4133363fe11af4f749297db8f">lighthouse performance report N 2</a><br>
+ <a href="https://googlechrome.github.io/lighthouse/viewer/?gist=23b8f92d04eb3f32f6cfd7e317535510">lighthouse performance report N 2</a><br>
  example N&deg;2 Emulated Nexus 5X, Throttled Fast 3G network,<br> Network throttling: 150 ms TCP RTT, 1.638,4 Kbps throughput (Simulated)<br>
  CPU throttling: 4x slowdown (Simulated):<br>
- <a href="https://googlechrome.github.io/lighthouse/viewer/?gist=8de61075ff1dec66617965bf5a51abc6">lighthouse performance report N 2</a><br><br>                                                                                                                       
+ <a href="https://googlechrome.github.io/lighthouse/viewer/?gist=b01c2975e469b0a2d36ea2224a78a84f">lighthouse performance report N 2</a><br><br>                                                                                                                       
                                                                                                                          
-<strong> SIMPLE CLASS USAGE EXAMPLE N&deg;1 FILE: INDEX.PHP (NO SIMULTANEOUSLY)</strong><br>                                                      
+<strong>SIMPLE CLASS USAGE EXAMPLE N&deg;1 FILE:INDEX.PHP (FONT-DISPLAY YES, NO SIMULTANEOUSLY CHECK,<br>                               
+ NO FONT FILES ARE STORED LOCALLY, .HTACCESS CACHE YES);</strong><br>                                                      
  $ref= new Fontperformance;<br>                                                                                              
  $font_1 = $ref->fontdisplay("link_to_font_api","fallback");<br>                                                             
  $font_2 = $ref->fontdisplay("link_to_font_api","auto");<br>               
@@ -168,14 +173,15 @@ width:100%;
  param 2 is a string, is the performance controlling option. Possible values are:<br>                                        
  auto | block | swap | fallback | optional<br><br>                                                                               
                                                                                                                          
-<strong> ADVANCED CLASS USAGE EXAMPLE N&deg;2 FILE: INDEX_2.PHP (SIMULTANEOUSLY)</strong><br>                                                     
+<strong>ADVANCED CLASS USAGE EXAMPLE N&deg;2 FILE:INDEX_2.PHP (FONT-DISPLAY YES, SIMULTANEOUSLY CHECK YES,<br>                          
+ ALL FONT FILES ARE STORED LOCALLY IF LOCALLY DO NOT EXIST, .HTACCESS CACHE YES);</strong><br>                                                     
  $ref= new Fontperformance;<br>                                                                                              
  $apilink = array("link_to_font_api_1","link_to_font_api_n", ....);<br>                                                      
  $ref->multi_simul_fontdisplay($apilink,"fallback" );<br>                                                                    
- where the params1 is an array with all links to the font api<br>                                                            
+ where the params1 is an array with all links to the font api,<br> it's good, also, for only one font,<br>                                                            
  and where the param 2 is a string, is the performance controlling option. Possible values are:<br>                                         
- auto | block | swap | fallback | optional , this will return an array with all fonts.<br>                                                                                
-<strong> PLEASE DO NOT FOLLOW MY BAD EXAMPLE, DON'T LOAD MORE THAN TWO MAXIMUM DIFFERENT FONTS SIMULTANEOUSLY</strong><br><br> 
+ auto | block | swap | fallback | optional , this will return an array with all fonts.                                                                                
+<br><br> 
         For a complete reference guide about font-display descriptors please consult:<br> 
         <a href="https://developers.google.com/web/updates/2016/02/font-display">Controlling Font Performance with font-display
         </a><br>
